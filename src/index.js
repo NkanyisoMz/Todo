bccf9340dc6ef1ddb9a6a32d06569f314c23fe79
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-09-08 20:27:32 +0200

    Add imports on index and load default projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './style.css';
+import "./style.css";
+import Project from "./modules/project.js";
+import Todo from "./modules/todo.js";
+import { renderProjects } from "./modules/dom.js";
+import { saveProjects, loadProjects } from "./modules/storage.js";
+
+// Loads or create default project
+let projects = loadProjects();
+if (projects.length === 0) {
+  projects.push(new Project("Default"));
+  saveProjects(projects);
+}
+
+// Render the UI
+renderProjects(projects);
